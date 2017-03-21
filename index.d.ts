a13db176649e8a62c0d4e87b51ad324d56d6aa60
Arran Bartish <abartish@teksystems.com>
2017-03-21 14:22:50 -0400

    add index

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright Arran Bartish. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/arranbartish/angular-cli-widgets/blob/master/LICENSE
+ */
+/**
+ * @module
+ * @description
+ * Entry point for all public APIs of the common package.
+ */
+export * from './src/app/widget';
