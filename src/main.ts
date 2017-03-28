b1f0ffb711852f2af4b0d5d1920538cce425d4d6
Arran Bartish <abartish@teksystems.com>
2017-03-28 16:29:03 -0400

    add package aot task

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {platformBrowser} from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+import { AppModuleNgFactory } from '../dist/aot/src/app/app.module.ngfactory';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
