0f3a126b35bf7b3ccc8bbb2b185557b2c04c330f
Arran Bartish <abartish@teksystems.com>
2017-04-04 14:25:11 -0400

    inline packaged resources with gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var inlineResources = require('./scripts/gulp/inline-resources');
+var sass = require('gulp-sass');
+
+
+gulp.task('copy-and-inline-resource', copyHtml);
+
+function copyHtml() {
+  gulp.src('src/app/**/*.html')
+    .pipe(gulp.dest('./dist/aot/src/app')).on('end', copyAssets);
+}
+
+function copyAssets () {
+  gulp.src('./src/assets/**/*')
+    .pipe(gulp.dest('./dist/aot/assets')).on('end', copyScss);
+}
+function copyScss () {
+  gulp.src('./src/app/**/*.scss')
+    .pipe(gulp.dest('./dist/aot/src/app')).on('end', inlineResource);
+}
+
+function inlineResource() {
+  inlineResources('./dist/aot/src/app');
+}
+
+gulp.task('default', ['copy-and-inline-resource']);
--- src/app/widget/navigation/navigation.component.ts
+++ src/app/widget/navigation/navigation.component.ts
@@ -5,7 +5,7 @@ import { TreeElement } from './valueObject/treeElement';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
     @Input()
