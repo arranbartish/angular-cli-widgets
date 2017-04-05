32a450f5ad6c6ad34e7e60442bfdfdb011df3ca2
Arran Bartish <abartish@teksystems.com>
2017-04-05 09:40:52 -0400

    move all aot resources to aot dist location and fix gulp processing

--- gulpfile.js
+++ gulpfile.js
@@ -7,20 +7,45 @@ gulp.task('copy-and-inline-resource', copyHtml);
 
 function copyHtml() {
   gulp.src('src/app/**/*.html')
-    .pipe(gulp.dest('./dist/aot/src/app')).on('end', copyAssets);
+    .pipe(gulp.dest('./aot/src/app')).on('end', copyAssets);
 }
 
 function copyAssets () {
   gulp.src('./src/assets/**/*')
-    .pipe(gulp.dest('./dist/aot/assets')).on('end', copyScss);
+    .pipe(gulp.dest('./aot/assets')).on('end', copyScss);
 }
 function copyScss () {
   gulp.src('./src/app/**/*.scss')
-    .pipe(gulp.dest('./dist/aot/src/app')).on('end', inlineResource);
+    .pipe(gulp.dest('./aot/src/app')).on('end', copyGitIgnoreFiles);
+}
+
+function copyGitIgnoreFiles () {
+  gulp.src('./**/.gitignore')
+    .pipe(gulp.dest('./aot')).on('end', copyNpmIgnoreFiles);
+}
+
+function copyNpmIgnoreFiles () {
+  gulp.src('./**/.npmignore')
+    .pipe(gulp.dest('./aot')).on('end', copyPackageJsonFile);
+}
+
+function copyPackageJsonFile () {
+  gulp.src('./package.json')
+    .pipe(gulp.dest('./aot')).on('end', copyLicenseFile);
+}
+
+function copyLicenseFile () {
+  gulp.src('./LICENSE')
+    .pipe(gulp.dest('./aot')).on('end', copyReadmeMdFile);
+}
+
+function copyReadmeMdFile () {
+  gulp.src('./README.md')
+    .pipe(gulp.dest('./aot')).on('end', inlineResource);
 }
 
 function inlineResource() {
-  inlineResources('./dist/aot/src/app');
+  inlineResources('./aot/src/app');
 }
 
 gulp.task('default', ['copy-and-inline-resource']);
--- src/main-aot.ts
+++ src/main-aot.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import {platformBrowser} from '@angular/platform-browser';
 
-import { WidgetModuleNgFactory } from '../dist/aot/src/app/widget/widget.module.ngfactory';
+import { WidgetModuleNgFactory } from '../aot/src/app/widget/widget.module.ngfactory';
 import { environment } from './environments/environment';
 
 if (environment.production) {
