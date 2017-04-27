a3e0e9dd732cd77f1583f3528f41326be301cffc
Florian GOMBERT <fgombert@TEKsystems.com>
2017-04-27 15:21:45 -0400

    Fixing ngRx mechanism around the navigation + upgrade package versions to match the consumer app's + commenting the fulp task (that used to downgrade some npm package versions at packaging time)

--- src/app/widget/navigation/actions/menu.action.ts
+++ src/app/widget/navigation/actions/menu.action.ts
@@ -5,7 +5,8 @@ import { TreeElement } from '../valueObject/treeElement';
 
 export const MenuAction = {
     LIST_MENU_ITEMS: type('Menu - list menu items'),
-    ADD_MENU_ITEMS: type('Menu - add menu items')
+    ADD_MENU_ITEMS: type('Menu - add menu items'),
+    INIT_MENU_ITEMS: type('Menu - initialize menu items')
 };
 
 export class MenuActionFactory {
@@ -15,6 +16,10 @@ export class MenuActionFactory {
 
     static addMenuItems(list: TreeElement[]): Action {
         return new AddMenuItemsAction(list);
+    }
+
+    static setMenuItems(list: TreeElement[]): Action {
+        return new SetMenuItemsAction(list);
     }
 }
 
@@ -34,6 +39,16 @@ export class AddMenuItemsAction implements Action {
 
     constructor(payload: TreeElement[]) {
         this.type = MenuAction.ADD_MENU_ITEMS;
+        this.payload = payload;
+    }
+}
+
+export class SetMenuItemsAction implements Action {
+    type: string;
+    payload?: any;
+
+    constructor(payload: TreeElement[]) {
+        this.type = MenuAction.INIT_MENU_ITEMS;
         this.payload = payload;
     }
 }
--- src/app/widget/navigation/reducer/menu.reducer.ts
+++ src/app/widget/navigation/reducer/menu.reducer.ts
@@ -4,8 +4,10 @@ import { MenuAction } from '../actions/menu.action';
 
 export const treeElements = (state: any = [], action: Action) => {
     switch (action.type) {
+        case MenuAction.INIT_MENU_ITEMS:
+            return action.payload;
         case MenuAction.ADD_MENU_ITEMS:
-            return [...action.payload];
+            return [...state, ...action.payload];
         case MenuAction.LIST_MENU_ITEMS:
         default:
             return state;
