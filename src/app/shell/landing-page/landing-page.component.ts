7769a243d48e954842311651ea807a63993b5537
autushka <aleh.autushka@gmail.com>
2017-04-29 18:40:08 -0400

    - landing-page titles added;

---
+++ src/app/shell/landing-page/landing-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent implements OnInit {
+  atomsSectionTitle = 'Atoms';
+  moleculesSectionTitle = 'Molecules';
+  organismsSectionTitle = 'Organisms';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
